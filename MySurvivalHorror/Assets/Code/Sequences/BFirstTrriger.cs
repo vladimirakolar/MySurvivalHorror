c48d5082e1a606c87de65fe4cd5aaa1ea0653d3c
Vladimira <vladimirakolar@gmail.com>
2020-02-12 15:16:03 +0100

    Created a trigger to advance the story, import some more objects and created a pointer to show the player where to go.

---
+++ MySurvivalHorror/Assets/Code/Sequences/BFirstTrriger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class BFirstTrriger : MonoBehaviour
+{
+    public GameObject ThePlayer;
+    public GameObject TextBox;
+    public GameObject TheMarker;
+
+    void OnTriggerEnter()
+    {
+        ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+        StartCoroutine(SceenePlayer());
+    }
+
+    IEnumerator SceenePlayer()
+    {
+        TextBox.GetComponent<Text>().text = "Looks like a weapon on that table";
+        yield return new WaitForSeconds(2.5f);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+        TheMarker.SetActive(true);
+    }
+}
