06ed71bca11c18214cdc5aee0c8746db8c6f77a6
Vladimira <vladimirakolar@gmail.com>
2020-02-13 20:00:17 +0100

    Builded up our weapon mechanics in preparation to complete our jumpscare.

---
+++ MySurvivalHorror/Assets/Code/FirePistol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePistol : MonoBehaviour
+{
+    public GameObject TheGun;
+    public GameObject MuzzelFlash;
+    public AudioSource GunFire;
+    public bool IsFiring = false;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (IsFiring==false)
+            {
+                StartCoroutine(FiringPistol());
+            }
+        }
+    }
+
+    IEnumerator FiringPistol()
+    {
+        IsFiring = true;
+        TheGun.GetComponent<Animation>().Play("PistolShot");
+        MuzzelFlash.SetActive(true);
+        MuzzelFlash.GetComponent<Animation>().Play("MuzzleAnim");
+        GunFire.Play();
+        yield return new WaitForSeconds(0.5f);
+        IsFiring = false;
+    }
+}
