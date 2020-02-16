0716cdc22894dcf00d5fb13dcd2adae294e46ce2
Vladimira <vladimirakolar@gmail.com>
2020-02-16 20:35:59 +0100

    Designed a splash screen with logo, added in better effects for our menu and linked things together.

---
+++ MySurvivalHorror/Assets/Code/Manu/SplashToMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashToMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(TakeToMenu());
+    }
+
+    IEnumerator TakeToMenu()
+    {
+        yield return new WaitForSeconds(7);
+        SceneManager.LoadScene(1);
+
+    }
+}
