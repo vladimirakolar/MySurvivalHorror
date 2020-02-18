8c249fd99037712dc0af0a9135a20fde89454447
Vladimira <vladimirakolar@gmail.com>
2020-02-18 16:49:04 +0100

    Bringed in a key and created the iconic glowing object

--- MySurvivalHorror/Assets/Code/Enviromment/VasaBreck.cs
+++ MySurvivalHorror/Assets/Code/Enviromment/VasaBreck.cs
@@ -7,6 +7,8 @@ public class VasaBreck : MonoBehaviour
     public GameObject fakeVase;
     public GameObject brockenVasa;
     public GameObject sfereObject;
+    public AudioSource brockVase;
+    public GameObject keyObject;
     
     public void DemageZombie(int DamageAmount)
     {
@@ -16,6 +18,8 @@ public class VasaBreck : MonoBehaviour
     IEnumerator BreakVase()
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        brockVase.Play();
+        keyObject.SetActive(true);
         fakeVase.SetActive(false);
         brockenVasa.SetActive(true);
         yield return new WaitForSeconds(0.05f);
--- MySurvivalHorror/Assets/Code/FirePistol.cs
+++ MySurvivalHorror/Assets/Code/FirePistol.cs
@@ -27,11 +27,13 @@ public class FirePistol : MonoBehaviour
     {
         RaycastHit Shot;
         IsFiring = true;
+
         if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),out Shot))
             {
                 TargetDistance = Shot.distance;
                 Shot.transform.SendMessage("DamageZombie", DamageAmout, SendMessageOptions.DontRequireReceiver);
             }
+
         TheGun.GetComponent<Animation>().Play("PistolShot");
         MuzzelFlash.SetActive(true);
         MuzzelFlash.GetComponent<Animation>().Play("MuzzleAnim");
